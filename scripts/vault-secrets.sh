bb92ee3e2947d87beb02cd6d8b5259a630345ac5
Ian Hewlett <ian.w.hewlett@gmail.com>
2024-11-10 16:37:57 -0800

    test vault plugin

--- scripts/vault-secrets.sh
+++ scripts/vault-secrets.sh
@@ -3,3 +3,5 @@ vault secrets enable -path=secret kv-v2
 vault kv put secret/svc/github \
   token_username="git" \
   image_pull_token="$GITHUB_TOKEN"
+vault secrets enable -path=avp kv-v2
+vault kv put avp/test sample=secret
