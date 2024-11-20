53d10c886fcc91b8313d6bd71ff40b3c51541787
Ian Hewlett <ian.w.hewlett@gmail.com>
2024-11-20 12:42:30 -0800

    refactor demo environment structure

--- environments/demo/bootstraph.sh
+++ environments/demo/bootstraph.sh
@@ -2,13 +2,13 @@
 set -eo pipefail
 
 
-kubectl apply -f environments/demo/argocd/argocd-ns.yaml
+kubectl apply -f environments/demo/mesh/argocd/argocd-ns.yaml
 
 kubectl create secret generic argocd-sync-secret -n argocd \
   --from-literal=git_username=username \
   --from-literal=git_token="$GH_PAT" \
   --dry-run=client -o yaml | kubectl apply -f -
 
-kubectl apply -k environments/demo/argocd
+kubectl apply -k environments/demo/mesh/argocd
 
-kubectl apply -k environments/demo/instances/demo-minikube-us-east-0
+kubectl apply -k environments/demo/clusters/demo-minikube-us-east-0
